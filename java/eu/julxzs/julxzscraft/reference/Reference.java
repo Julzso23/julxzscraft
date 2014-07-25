77f1e0a773938dafe90386620fc17b91b96e12dd
Julzso23 <julian@julxzs.eu>
2014-07-25 23:45:08 +0100

    Added a reference class for holding constants

---
+++ java/eu/julxzs/julxzscraft/reference/Reference.java
@@ -0,0 +1,8 @@
+package eu.julxzs.julxzscraft.reference;
+
+public class Reference
+{
+	public static final String MOD_ID = "JulxzsCraft";
+	public static final String MOD_NAME = "JulxzsCraft";
+	public static final String VERSION = "1.7.10-1.0.0";
+}
