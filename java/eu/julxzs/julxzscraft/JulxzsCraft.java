676a37abb3e2a125521c34cfebcaa33247d3d534
Julzso23 <julian@julxzs.eu>
2014-07-25 23:27:10 +0100

    Added sidedproxy object

--- java/eu/julxzs/julxzscraft/JulxzsCraft.java
+++ java/eu/julxzs/julxzscraft/JulxzsCraft.java
@@ -1,15 +1,20 @@
 package eu.julxzs.julxzscraft;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import eu.julxzs.julxzscraft.proxy.IProxy;
 
 @Mod(modid = "JulxzsCraft", name = "JulxzsCraft", version = "1.7.10-1.0.0")
 public class JulxzsCraft
 {
 	@Mod.Instance("JulxzsCraft")
 	public static JulxzsCraft instance;
+
+	@SidedProxy(clientSide = "eu.julxzs.julxzscraft.proxy.ClientProxy", serverSide = "eu.julxzs.julxzscraft.proxy.ServerProxy")
+	public static IProxy proxy;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)
