ac2992930e6c981afcc6ffc858cb8859a314b405
Julzso23 <julian@julxzs.eu>
2014-07-25 21:50:37 +0100

    Added empty mod class

---
+++ java/eu/julxzs/julxzscraft/JulxzsCraft.java
@@ -0,0 +1,25 @@
+package eu.julxzs.julxzscraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid = "JulxzsCraft", name = "JulxzsCraft", version = "1.7.10-1.0.0")
+public class JulxzsCraft
+{
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	}
+
+	@Mod.EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+	}
+}
