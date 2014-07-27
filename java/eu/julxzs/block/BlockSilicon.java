5f68e806e5a176b2e47895ca928a1da31f66a889
Julzso23 <julian@julxzs.eu>
2014-07-27 14:22:37 +0100

    Added Silicon block

---
+++ java/eu/julxzs/block/BlockSilicon.java
@@ -0,0 +1,12 @@
+package eu.julxzs.block;
+
+import net.minecraft.block.material.Material;
+
+public class BlockSilicon extends BlockJC
+{
+	public BlockSilicon()
+	{
+		super(Material.iron);
+		this.setBlockName("siliconBlock");
+	}
+}
