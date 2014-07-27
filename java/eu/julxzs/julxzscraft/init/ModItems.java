6fe60dfb3f2eacaa05932238de63ecfb3a8c309d
Julzso23 <julian@julxzs.eu>
2014-07-27 21:24:08 +0100

    Added new items to the moditems class

--- java/eu/julxzs/julxzscraft/init/ModItems.java
+++ java/eu/julxzs/julxzscraft/init/ModItems.java
@@ -1,15 +1,21 @@
 package eu.julxzs.julxzscraft.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import eu.julxzs.julxzscraft.item.ItemBreadSlice;
 import eu.julxzs.julxzscraft.item.ItemJC;
 import eu.julxzs.julxzscraft.item.ItemSilicon;
+import eu.julxzs.julxzscraft.item.ItemToast;
 
 public class ModItems
 {
 	public static final ItemJC silicon = new ItemSilicon();
+	public static final ItemJC breadSlice = new ItemBreadSlice();
+	public static final ItemJC toast = new ItemToast();
 
 	public static void init()
 	{
 		GameRegistry.registerItem(silicon, "silicon");
+		GameRegistry.registerItem(breadSlice, "breadSlice");
+		GameRegistry.registerItem(toast, "toast");
 	}
 }
