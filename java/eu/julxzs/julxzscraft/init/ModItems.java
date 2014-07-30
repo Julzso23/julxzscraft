baf2e1fc9adbacd9ad0c77f9d1fd44f55766d5fe
Julzso23 <julian@julxzs.eu>
2014-07-30 23:03:01 +0100

    Added iron ring

--- java/eu/julxzs/julxzscraft/init/ModItems.java
+++ java/eu/julxzs/julxzscraft/init/ModItems.java
@@ -6,12 +6,14 @@ import eu.julxzs.julxzscraft.item.*;
 public class ModItems
 {
 	public static final ItemJC silicon = new ItemSilicon();
+	public static final ItemJC ironRing = new ItemIronRing();
 	public static final ItemFoodJC breadSlice = new ItemBreadSlice();
 	public static final ItemFoodJC toast = new ItemToast();
 
 	public static void init()
 	{
 		GameRegistry.registerItem(silicon, "silicon");
+		GameRegistry.registerItem(ironRing, "ironRing");
 		GameRegistry.registerItem(breadSlice, "breadSlice");
 		GameRegistry.registerItem(toast, "toast");
 	}
---
+++ java/eu/julxzs/julxzscraft/item/ItemIronRing.java
@@ -0,0 +1,10 @@
+package eu.julxzs.julxzscraft.item;
+
+public class ItemIronRing extends ItemJC
+{
+	public ItemIronRing()
+	{
+		super();
+		this.setUnlocalizedName("ironRing");
+	}
+}
