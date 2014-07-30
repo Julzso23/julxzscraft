e6f310bc715de67f5c217e2c49cbb3e400e3640a
Julzso23 <julian@julxzs.eu>
2014-07-30 23:03:34 +0100

    Changed bread slice recipe to be bread cut with shears

--- java/eu/julxzs/julxzscraft/init/Recipes.java
+++ java/eu/julxzs/julxzscraft/init/Recipes.java
@@ -3,6 +3,8 @@ package eu.julxzs.julxzscraft.init;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class Recipes
@@ -10,7 +12,8 @@ public class Recipes
 	public static void init()
 	{
 		GameRegistry.addRecipe(new ItemStack(ModBlocks.siliconBlock), "sss", "sss", "sss", 's', new ItemStack(ModItems.silicon));
-		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.breadSlice, 5), new ItemStack(Items.bread));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.ironRing, 4), " i ", "i i", " i ", 'i', "ingotIron"));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.breadSlice, 5), new ItemStack(Items.bread), Items.shears.setContainerItem(Items.shears));
 		GameRegistry.addSmelting(new ItemStack(ModItems.breadSlice), new ItemStack(ModItems.toast), 0);
 	}
 }
